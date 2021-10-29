ee1ca9a9631b7ffea9ee1bc0820b58a7d72e04a3
Alex <alexanderesolis@gmail.com>
2021-10-28 21:15:01 -0400

    got MeepMeep working

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,31 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        // TODO: If you experience poor performance, enable this flag
+        // System.setProperty("sun.java2d.opengl", "true");
+
+        // Declare a MeepMeep instance
+        // With a field size of 800 pixels
+        MeepMeep mm = new MeepMeep(800)
+                // Set field image
+                .setBackground(MeepMeep.Background.FIELD_FREIGHT_FRENZY)
+                // Set theme
+                .setTheme(new ColorSchemeRedDark())
+                // Background opacity from 0-1
+                .setBackgroundAlpha(1f)
+                // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                .splineToSplineHeading(new Pose2d(-15, -24, Math.toRadians(180)), Math.toRadians(0))
+                                .splineToSplineHeading(new Pose2d(60, -50, Math.toRadians(-90)), Math.toRadians(270))
+                                .build()
+                )
+                .start();
+    }
+}
